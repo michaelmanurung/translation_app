0186962beaa70f5fad6b7d2f8c095d56f380bfde
Michael Manurung <6181801027@student.unpar.ac.id>
2021-06-13 20:02:13 +0700

    add translate function

--- index.php
+++ index.php
@@ -4,6 +4,30 @@
         <!-- Bootstrap library -->
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous">
 
+        <?php 
+            require_once ('vendor/autoload.php');
+            use \Statickidz\GoogleTranslate;
+            
+            //$source = 'en';
+            //$target = 'id';
+            //$text = 'Im on vacation';
+
+            //$trans = new GoogleTranslate();
+            //$result = $trans->translate($source, $target, $text);
+            $result = '...';
+
+            if(isset($_GET['buttonTrans'])){
+                $source = 'en';
+                $target = 'id';
+                $text = $_GET['inputTranslate'];
+
+                $trans = new GoogleTranslate();
+                $result = $trans->translate($source, $target, $text);
+                //echo '<h2>' .$result. '</h2>';
+                //echo '<h4>CLICKED</h4>';
+            } 
+        ?>
+
     </head>
     <body>
 
@@ -18,46 +42,32 @@
 
 
         <div class="position-absolute top-50 start-50 translate-middle">
-            <form>
+            <form name="form" action="" method="get">
               <div class="mb-3">
                 <label for="inputTranslate" class="form-label">Input Text</label>
-                <input type="text" class="form-control" id="inputTranslate" aria-describedby="emailHelp">
+                <input type="text" class="form-control" id="inputTranslate" name="inputTranslate" aria-describedby="emailHelp">
               </div>
               <div class="mb-3">
                 <label for="translatedText" class="form-label">Translated Text</label>
-                <input type="text" class="form-control" id="translatedText" disabled>
+                <input type="text" class="form-control" id="translatedText" name="translatedText" value="<?php echo $result ?>" disabled>
               </div>
               <div class="mb-3 form-check">
                 <label for="langType" class="form-label">Language translate</label>
-                <select class="form-select form-select-sm" aria-label=".form-select-sm example">
+                <select class="form-select form-select-sm" aria-label=".form-select-sm example" name="langType">
                   <option selected>Indonesia</option>
                   <option value="1">Mandarin</option>
                   <option value="2">Germany</option>
                   <option value="3">Thailand</option>
                 </select>
               </div>
-              <button type="submit" class="btn btn-primary">Submit</button>
+              <button type="submit" name="buttonTrans"
+                value="buttonTrans" class="btn btn-primary">Submit</button>
             </form>   
         </div>
 
         <div class="position-absolute bottom-0 end-0">
             <button type="button" class="btn btn-info">Help ?</button>
         </div>
-        
 
-        <?php 
-            require_once ('vendor/autoload.php');
-            use \Statickidz\GoogleTranslate;
-            
-            $source = 'en';
-            $target = 'id';
-            $text = 'Im on vacation';
-            
-            $trans = new GoogleTranslate();
-            $result = $trans->translate($source, $target, $text);
-            
-            // Good morning
-            echo '<h2>' .$result. '</h2>';
-        ?>
     </body>
 </html>
